b8e067dbc9d9973e40387aa3fdbef3c9004d5741
yangguang26 <yangguang26@maoyan.com>
2020-08-05 11:52:32 +0800

    完成时间筛选样式

--- components/filterPanel/index.js
+++ components/filterPanel/index.js
@@ -1,3 +1,20 @@
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1990; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
 Component({
   properties: {
     filterShow: {
@@ -53,9 +70,24 @@ Component({
         checked: "",
         value: 'custom',
       },
-    ]
+    ],
+    years: years,
+    year: date.getFullYear(),
+    months: months,
+    month: 2,
+    days: days,
+    day: 2,
+    value: [9999, 1, 1],
   },
   methods: {
+    bindChange: function (e) {
+      const val = e.detail.value
+      this.setData({
+        year: this.data.years[val[0]],
+        month: this.data.months[val[1]],
+        day: this.data.days[val[2]]
+      })
+    },
     dateSelectEvent: function (e) {
       const { value } = e.target.dataset;
       const { dateSet } = this.data;
