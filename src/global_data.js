b632c3cb2392597de77d216f0f025ed6710bda83
wangxiaojing05@maoyan.com <wangxiaojing05@maoyan.com>
2021-03-01 23:17:36 +0800

    fix

--- src/global_data.js
+++ src/global_data.js
@@ -3,17 +3,59 @@ import envConfig from "./constant/env-config.js";
 import reqPacking from "./utils/reqPacking.js";
 
 const globalData = {
-    APISet: envConfig,
-    reqPacking,
-    capsuleLocation: Taro.getMenuButtonBoundingClientRect(),
-    systemInfo: Taro.getSystemInfoSync(),
-  }
-
-  console.log(globalData.capsuleLocation);
-if(globalData.capsuleLocation.top===0){
-  globalData.capsuleLocation = { width: 87, height: 32, top: 24, left: 278, right: 365, bottom: 56 };
+  APISet: envConfig,
+  reqPacking,
+  capsuleLocation: null,
+  systemInfo: Taro.getSystemInfoSync(),
 }
-console.log('更正后为', globalData.capsuleLocation);
+
+let rect = null;
+try {
+  rect = Taro.getMenuButtonBoundingClientRect ? Taro.getMenuButtonBoundingClientRect() : null;
+  if (rect === null) {
+    throw 'getMenuButtonBoundingClientRect error';
+  }
+  //取值为0的情况
+  if (!rect.width) {
+    throw 'getMenuButtonBoundingClientRect error';
+  }
+  //取值为0的情况
+  if (!rect.width) {
+    throw 'getMenuButtonBoundingClientRect error';
+  }
+} catch (error) {
+  let gap = ''; //胶囊按钮上下间距 使导航内容居中
+  let width = 96; //胶囊的宽度，android大部分96，ios为88
+  if (systemInfo.platform === 'android') {
+    gap = 8;
+    width = 96;
+  } else if (systemInfo.platform === 'devtools') {
+    if (ios) {
+      gap = 5.5; //开发工具中ios手机
+    } else {
+      gap = 7.5; //开发工具中android和其他手机
+    }
+  } else {
+    gap = 4;
+    width = 88;
+  }
+  if (!systemInfo.statusBarHeight) {
+    //开启wifi的情况下修复statusBarHeight值获取不到
+    systemInfo.statusBarHeight = systemInfo.screenHeight - systemInfo.windowHeight - 20;
+  }
+  rect = {
+    //获取不到胶囊信息就自定义重置一个
+    bottom: systemInfo.statusBarHeight + gap + 32,
+    height: 32,
+    left: systemInfo.windowWidth - width - 10,
+    right: systemInfo.windowWidth - 10,
+    top: systemInfo.statusBarHeight + gap,
+    width: width
+  };
+}
+globalData.capsuleLocation = rect;
+
+
 export function set (key, val) {
   globalData[key] = val
 }
