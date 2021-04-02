8c6fb4ab86712ddc03409e253ef73269102e1399
王晓婧 <wangxiaojing05@maoyan.com>
2021-04-02 14:29:50 +0800

    fix 星期

--- src/pages/detail/competeMarket/index.js
+++ src/pages/detail/competeMarket/index.js
@@ -32,10 +32,8 @@ const CompeteMarket=(props)=>{
     const { releaseTime = {} } = props;
     const releaseTimeArry = releaseTime.time && releaseTime.time.match(/-/g);
     if ((releaseTimeArry && releaseTimeArry.length === 2)) {
-      // 获取该周的第几天，0-6, 0是星期天，所以需要做转换
-      let index = dayjs(releaseTime.time).day();
-      index = index===0 ? 7 : index+1;
-      console.log(index);
+      // 获取该周的第几天，0-6, 0是星期天
+      let index = dayjs(releaseTime.time).format('d') || 7;
       // 自然周的周一到周日
       const natureStartDate = +dayjs(releaseTime.time).subtract(index - 1, 'days');
       const natureEndDate = +dayjs(releaseTime.time).add(7 - index, 'days');
