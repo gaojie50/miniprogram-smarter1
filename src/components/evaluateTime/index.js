23da9edb17fe943e2f75fa41ea9cdb40a9fa0357
yangguang26 <yangguang26@maoyan.com>
2021-04-28 14:37:26 +0800

    count down is done

--- src/components/evaluateTime/index.js
+++ src/components/evaluateTime/index.js
@@ -1,51 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Taro from '@tarojs/taro';
-import { View, Image, Text } from '@tarojs/components';
+import { View, Text } from '@tarojs/components';
 import dayjs from 'dayjs';
 import utils from '@utils'
 import './index.scss';
 
 const { calcWeek } = utils;
 export default function EvaluateTime({ deadLine }) {
-  deadLine = 1619510751350;
-  const noLimit = deadLine == undefined;
-  let [isEnd, setIsEnd] = useState(noLimit ? false : (+new Date() >= deadLine));
-  const [seconds, setSeconds] = useState(0);
+  deadLine = +new Date() + 65 * 1000;
+  let now = +new Date();
+  const [timing, setTiming] = useState(false);
+  const [line,setLine] = useState(deadLine);
+  const [seconds, setSeconds] = useState(Math.floor((line - now) / 1000));
+  const noLimit = line == undefined;
 
-  function timeRender() {
-    const now = +new Date();
-    const timeFormat = timeStamp => {
-      const dayObj = dayjs(timeStamp);
-      const isToday = dayjs(timeStamp).isToday();
-      let interval = 1;
-      const exactSeconds = Math.floor((deadLine - now) / 1000);
+  let [isEnd, setIsEnd] = useState(noLimit ? false : (now >= line));
+  
+  if(!noLimit && !isEnd && !timing) setTiming(true);
 
-      if (exactSeconds >= 0) setTimeout(() => setSeconds(exactSeconds));
-
-      if (exactSeconds <= 0) setTimeout(() => setIsEnd(true));
-      if (exactSeconds >= 10 * 60) interval = 5 * 60;
-      if (2 * 60 < exactSeconds && exactSeconds < 10 * 60) interval = 60;
-
-      if (exactSeconds > 0) setTimeout(timeRender, interval * 1000);
-      
-      if ((exactSeconds <= 60) && exactSeconds > 0) return <Text className="red">{seconds}秒后</Text>
-      if (isToday) return <Text className="gray">今天{dayObj.format("HH:mm")}</Text>;
-      return <Text className="gray">{dayObj.format("M月D日")}/{calcWeek(timeStamp)} {dayObj.format("HH:MM")}</Text>;
-    };
-
-    return <React.Fragment>
-      {timeFormat(deadLine)}
-      <Text>
-        {isEnd ? '评估已结束' : '评估结束'}
-        {!isEnd && <Text className="arrow" />}
-      </Text>
-    </React.Fragment>
+  function modal(innerline) {
+    setTiming(false);
+    const newLine = +new Date() + 65 * 1000;
+    setLine(newLine);
+    setSeconds(Math.floor((newLine - new Date()) / 1000));
   };
 
-  return <View className={`evaluate-time ${isEnd}`}>
+  useEffect(() => {
+    let interval
+  
+    if (timing) {
+      interval = setInterval(() => {
+        setSeconds(preSecond => {
+          if (preSecond <= 1) {
+            setTiming(false);
+            setIsEnd(true);
+            clearInterval(interval)
+
+            return 0
+          } else {
+            return preSecond - 1
+          }
+        })
+      },1000)
+    }
+
+    return () => clearInterval(interval)
+  }, [timing])
+  const dayObj = dayjs(line);
+  const isToday = dayjs(line).isToday();
+
+  const timeFormat = timeStamp => {
+    if (timing && seconds<=60) return <Text className='red'>{seconds}秒后</Text>
+    if (isToday) return <Text className='gray'>今天{dayObj.format("HH:mm")}</Text>;
+    return <Text className='gray'>{dayObj.format("M月D日")}/{calcWeek(timeStamp)} {dayObj.format("HH:MM")}</Text>;
+  };
+
+  function timeRender(){
+  return <React.Fragment>
+    {timeFormat(line)}
+    <Text>
+      {isEnd ? '评估已结束' : '评估结束'}
+      {!isEnd && <Text className='arrow' />}
+    </Text>
+  </React.Fragment>
+  };
+
+  return <View className={`evaluate-time ${isEnd}`} onClick={isEnd ? '' : () => modal(line)}>
     {
       noLimit ?
-        <Text className="one-line">不限时评估<Text className="arrow" /></Text> :
+        <Text className='one-line'>不限时评估<Text className='arrow' /></Text> :
         timeRender()
     }
   </View >
