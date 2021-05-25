2bd2565080f60932f820ab5fb1f4c31d11e97f33
lanting08 <lanting08@maoyan.com>
2021-05-25 16:57:24 +0800

    bugfix+分页

--- src/pages/assessList/evaluate.js
+++ src/pages/assessList/evaluate.js
@@ -1,26 +1,12 @@
 import Taro, { useDidShow } from '@tarojs/taro';
 import { View, Image, Text, Button } from '@tarojs/components';
-import React, { useEffect, useMemo, useState, useRef, forwardRef } from 'react';
+import React, { useEffect, useMemo, useState, useRef } from 'react';
 import { noDataPic, defaultMovieCover as Cover } from '@utils/imageUrl';
 import dayjs from 'dayjs';
 import './evaluate.scss';
 import utils from '../../utils';
 import reqPacking from '../../utils/reqPacking';
 import useDeadline from '../assess/detail/useDeadline';
-// import VirtualList from '@tarojs/components/virtual-list';
-
-// function buildData (offset = 0) {
-//   return Array(100).fill(0).map((_, i) => i + offset);
-// }
-
-// const Row = React.memo(({ id, index, style, data }) => {
-//   console.log(id, index, style, data, 1111)
-//   return (
-//     <View id={id} className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
-//       Row {index} : {data[index]}
-//     </View>
-//   );
-// })
 
 
 const { formatNumber, isDockingPerson } = utils;
@@ -32,18 +18,19 @@ const TYPE = {
 }
 
 const TYPE_MOVIE = 3 || 4;
-// const DEFAULT_PROJECT_ROLE = 6;
-const EvalutaionCard = forwardRef(_EvalutaionCard);
 
-export function EvaluationList({type}, ref) {
+export function EvaluationList({type, offset}) {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
+  const [fetchLoading, setFetchLoading] = useState(false);
+  const [noData, setNodata] = useState(false);
   const fetch = useRef(false);
-  // const [testData, setTestData] = useState(buildData(0));
 
   useDidShow(() => {
-    setLoading(true)
-    fetchEvalutaionData(type)
+    setLoading(true);
+    setNodata(false);
+    setData({});
+    fetchEvalutaionData(type, 0)
     .then(res => {
       const { success, error } = res;
       if(success) {
@@ -69,8 +56,10 @@ export function EvaluationList({type}, ref) {
       fetch.current = true;
       return
     }
-    setLoading(true)
-    fetchEvalutaionData(type)
+    setData({});
+    setNodata(false);
+    setLoading(true);
+    fetchEvalutaionData(type, 0)
     .then(res => {
       const { success, error } = res;
       if(success) {
@@ -91,26 +80,56 @@ export function EvaluationList({type}, ref) {
     })
   }, [type])
 
+  useEffect(() => {
+    if(!fetch.current) {
+      fetch.current = true;
+      return
+    }
+
+    if(offset === 0 || noData) {
+      return
+    }
+    setFetchLoading(true)
+    fetchEvalutaionData(type, offset)
+    .then(res => {
+      const { success, error } = res;
+      if(success) {
+        if(!res.data.evaluationList || res.data.evaluationList.length === 0) {
+          setNodata(true);
+          return
+        }
+        const newData = JSON.parse(JSON.stringify(data));
+        newData.evaluationList = data.evaluationList.concat(res.data.evaluationList);
+        setData(newData || {})
+      } else {
+        Taro.showToast({
+          title: error.message,
+          icon: 'none'
+        })
+      }
+      setFetchLoading(false)
+    })
+    .catch(err => {
+      Taro.showToast({
+        title: err,
+        icon: 'none'
+      })
+    })
+  }, [offset])
+
   const [evaluationList] = useMemo(() => {
     const { evaluationList: __evaluationList = [] } = data;
+
     return [__evaluationList];
   }, [data])
 
   return loading ? <mpLoading show type='circle' tips=''></mpLoading> :
           evaluationList?.length > 0 ? <>
-          {/* <VirtualList
-            height={500} 
-            width='100%'
-            itemData={testData} 
-            itemCount={testData.length} 
-            itemSize={20} 
-          >
-            {Row} 
-          </VirtualList> */}
           {
-            evaluationList.map((item, index) => <EvalutaionCard key={index} {...item} ref={ref} />)
+            evaluationList.map((item, index) => <EvalutaionCard key={index} {...item} />)
           }
-          <View className='assess-list-content-body-noMore'>没有更多了</View>
+          {fetchLoading && !noData && (<View><mpLoading show type='circle' /></View>)}
+          {noData && (<View className='assess-list-content-body-noMore'>没有更多了</View>)}
           </> : (
           <>
             <View className='no-eval-data'>
@@ -121,13 +140,13 @@ export function EvaluationList({type}, ref) {
           )
 }
 
-function _EvalutaionCard(props, ref) {
+function EvalutaionCard(props) {
   const [realName, setRealName] = useState('');
 
   const {
     category,
     round = '-', participantNumber,
-    roundTitle, startDate, evaluationMethod, evaluationTotalScore,
+    roundTitle, evaluationMethod, evaluationTotalScore,
     estimateBox, estimateScore, initiator = '-', projectId, roundId,
     hasAssess, invitees,
     projectRole,
@@ -140,17 +159,6 @@ function _EvalutaionCard(props, ref) {
   if(deadline === 0) {
     deadline = null;
   }
-
-  // const timeStr = useMemo(() => {
-  //   if (!startDate) return '-'
-  //   const time = new Date(startDate);
-  //   const d = time.getDate();
-  //   const h = time.getHours();
-  //   const m = time.getMinutes();
-  //   const s = time.getSeconds();
-  //   const str = `${time.getFullYear()}-${time.getMonth() + 1}-${d < 10 ? `0${d}` : d} ${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`
-  //   return str;
-  // }, [startDate])
   
 
   const arr = useMemo(() => {
@@ -314,20 +322,22 @@ function _EvalutaionCard(props, ref) {
         {isDockingPerson(role) && judgeDeadLine(deadline) && <Button
           data-roundTitle={roundTitle}
           data-roundId={roundId}
+          data-projectId={projectId}
+          data-pic={imageUrl?.replace('/w.h', '')}
           data-sign='invite'
           openType='share'
           className='assess-list-evaluation-card-action-btn'
-          onClick={() => ref.current = props}
         >
           邀请参与
         </Button>}
         <Button
           data-roundTitle={roundTitle}
           data-roundId={roundId}
+          data-projectId={projectId}
+          data-pic={imageUrl?.replace('/w.h', '')}
           data-sign='attend' 
           openType='share'
           className='assess-list-evaluation-card-action-btn'
-          onClick={() => ref.current = props}
         >
           分享结果
         </Button>
@@ -357,7 +367,7 @@ function judgeInvitee(invitees, realName) {
   return typeof invitees === 'string' && invitees.includes(realName)
 }
 
-export function fetchEvalutaionData(type) {
+export function fetchEvalutaionData(type, offset) {
   const { userInfo } = Taro.getStorageSync('authinfo');
   return new Promise(resolve => {
     reqPacking({
@@ -365,8 +375,8 @@ export function fetchEvalutaionData(type) {
       data: {
         type: type + 1,
         userId: userInfo.id,
-        // offset: 0,
-        // limit: 10
+        offset,
+        limit: 10
       }
     }, 'server').then(res => {
       resolve(res)
